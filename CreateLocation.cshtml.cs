a8f1fa6a5e5201ede1c2473a5a18b260738f861f
AbodMilad <99974543+AbodMilad@users.noreply.github.com>
2022-02-18 16:31:36 +0200

    Add files via upload

---
+++ CreateLocation.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kanusaoft1.Views.Home
+{
+    public class CreateLocationModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
